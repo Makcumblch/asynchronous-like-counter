60d4c58cca210092639d2b5a4855717097944c0c
Makcumblch <maxim.fedorov28@yandex.ru>
2024-08-24 15:30:05 +0500

    Add CORS MW

---
+++ server/internal/infra/http/middleware/cors.go
@@ -0,0 +1,15 @@
+package middleware
+
+import "net/http"
+
+type Middleware func(http.Handler) http.Handler
+
+func CorsMW(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		handler.ServeHTTP(w, r)
+	})
+}
